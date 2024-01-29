ec1498fc5f1b2d494a35e9153bbae1e62643746e
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-01-29 22:30:20 +0900

    Improve: getCurrentPosition 함수에 mocking

--- __tests__/geolocation2.test.ts
+++ __tests__/geolocation2.test.ts
@@ -1,5 +1,5 @@
 import { useGeolocation } from '@application/hooks/location';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 
 describe('[Hooks] useGeolocation', () => {
   // 윈도우 환경이 아니기 때문에 관련 메서드나 함수를 테스트 전에 직접 mock으로 만듦
@@ -15,16 +15,15 @@ describe('[Hooks] useGeolocation', () => {
   });
 
   // 1. default 확인
-  test('현재 location(isLoading, coords, errorMessage)을 반환한다', () => {
+  test('defalut value를 반환한다', () => {
     const { result } = renderHook(() => useGeolocation());
     expect(result.current.location.isLoading).toBe(false);
-    expect(result.current.location.coords).toBe(undefined);
-    expect(result.current.location.errorMessage).toBe('');
+    expect(result.current.location.data).toBe(undefined);
+    expect(result.current.location.error).toBe(undefined);
   });
 
-  // 2. 값이 변경됐을 때 확인
+  // 2-1. mock data 생성해서 값 변경
   test('location의 coords 객체 값만 변경', () => {
-    // mock data 생성해서 값 변경
     const mockPosition = { coords: { latitude: 1, longitude: 2 } };
     mockGeolocation.getCurrentPosition.mockImplementation((callback) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -32,26 +31,32 @@ describe('[Hooks] useGeolocation', () => {
     });
     const { result } = renderHook(() => useGeolocation());
 
-    expect(result.current.location.coords?.latitude).toBe(1);
-    expect(result.current.location.coords?.longitude).toBe(2);
+    act(() => {
+      result.current.getCurrentPosition();
+    });
+
+    expect(result.current.location.data?.coords.latitude).toBe(1);
+    expect(result.current.location.data?.coords.longitude).toBe(2);
   });
 
-  // 3. 값이 변경됐을 때 확인
+  // 2-2. mock data 생성해서 값 변경
   test('location의 errorMessage 값만 있고 isLoading는 false, coords는 undefined로 변경', () => {
-    // mock data 생성해서 값 변경
-    const mockPosition = {
-      errorMessage: 'denied geolocation',
-      isLoading: 'false',
-      coords: undefined,
-    };
-    mockGeolocation.getCurrentPosition.mockImplementation((callback) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      callback(mockPosition);
-    });
+    const errorMessage = 'denied geolocation';
+    // getCurrentPosition의 스펙에 맞춰 콜백 함수 작성
+    mockGeolocation.getCurrentPosition.mockImplementation(
+      (successCallback, errorCallback) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        errorCallback(errorMessage);
+      },
+    );
     const { result } = renderHook(() => useGeolocation());
 
-    expect(result.current.location.errorMessage).toBe('denied geolocation');
+    act(() => {
+      result.current.getCurrentPosition();
+    });
+
+    expect(result.current.location.error).toBe(errorMessage);
     expect(result.current.location.isLoading).toBe(false);
-    expect(result.current.location.coords).toBe(undefined);
+    expect(result.current.location.data).toBe(undefined);
   });
 });
