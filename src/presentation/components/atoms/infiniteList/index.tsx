7166619204a0ccc42cdb73b712dad32c4486cd3e
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-01-30 18:08:19 +0900

    Comment: infiniteList 개선점 작성

--- src/presentation/components/atoms/infiniteList/index.tsx
+++ src/presentation/components/atoms/infiniteList/index.tsx
@@ -1,4 +1,9 @@
-import { ComponentType, CSSProperties, useCallback, useMemo } from 'react';
+import {
+  ComponentType,
+  CSSProperties,
+  useCallback,
+  useMemo,
+} from 'react';
 import {
   AutoSizer,
   InfiniteLoader,
@@ -30,7 +35,6 @@ export interface InfiniteListProps<T> {
   bottomLoaderRender?: ComponentType;
 }
 
-
 export function InfiniteList<T>({
   data,
   totalElements, // 추가하면 좋은 속성
@@ -43,24 +47,35 @@ export function InfiniteList<T>({
   fetchNextPage,
   bottomLoaderRender: LoaderComponent,
 }: InfiniteListProps<T>) {
-
+  // hasNext
   const rowCount = useMemo(() => {
-    return data.length;
-  }, [data]);
+    // loader를 list 범위에 추가하기 위해서 hasNextPage를 체크해서 true면 1을 추가함
+    // 마지막 index = last one은 항상 loader
+    return data.length + {hasNextPage ? 1 : 0};
+  }, [data, hasNextPage]);
 
   const onRowRenderer = useCallback(
     (props: ListRowProps): JSX.Element => {
-      return (
-        <RowComponent
-          key={props.index}
-          index={props.index}
-          item={data[props.index]!}
-          style={props.style}
-        />
-      );
+      // props.index의 마지막값이 undefined인 경우에만 bottomloader를 렌더 
+      // 왜냐면? 원래 index.length에서 loader를 추가하기 위해 +1를 했기 때문에 마지막 인덱스의 값은 undefined임.
+      if (data.length === data[props.index] + 1) {
+        return (
+          <RowComponent
+            key={props.index}
+            index={props.index}
+            item={data[props.index]!}
+            style={props.style}
+          />
+        );
+      }
+      return <LoaderComponent />;
     },
     [data],
   );
+
+  // loadMoreRows를 추가하여 무조건적으로 fetchNextPage가 실행되지 않도록 함 
+  // 이유는> 불필요한 상황에서도 렌더링될 수 있음 최소한의 에러도 없애기 위해서 
+  // hasNextPage와 isFetchingNextPage를 체크해서 fetchNextPage가 실행되어야 하는 상황을 체크해서 조건부로 렌더링함
 
   const onEmptyRenderer = useCallback((): JSX.Element => {
     if (EmptyComponent) {
@@ -77,17 +92,16 @@ export function InfiniteList<T>({
   }, [LoaderComponent]);
 
   const isRowLoaded = useCallback(
-    ({index}: {index: number}): boolean => {
+    (index: number): boolean => {
       return !!data[index];
     },
     [data],
   );
 
-
   return (
-    <>
-      <WindowScroller>
-        {({ height, isScrolling, scrollTop }) => (
+    <WindowScroller>
+      {({ height, isScrolling, scrollTop }) => (
+        <>
           <InfiniteLoader
             isRowLoaded={isRowLoaded}
             loadMoreRows={fetchNextPage}
@@ -113,10 +127,10 @@ export function InfiniteList<T>({
               </AutoSizer>
             )}
           </InfiniteLoader>
-        )}
-      </WindowScroller>
-      {isFetchingNextPage && onLoaderRenderer}
-    </>
+          {isFetchingNextPage && onLoaderRenderer}
+        </>
+      )}
+    </WindowScroller>
   );
 }
 
