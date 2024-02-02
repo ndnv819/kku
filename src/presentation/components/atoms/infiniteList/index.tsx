878c713273725f415ea3587ecb7c82ef38a3f865
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-02-02 21:29:09 +0900

    Improve: InfiniteList의 List 내부에 bottomLoader가 포함되도록 수정

--- src/presentation/components/atoms/infiniteList/index.tsx
+++ src/presentation/components/atoms/infiniteList/index.tsx
@@ -1,11 +1,7 @@
-import {
-  ComponentType,
-  CSSProperties,
-  useCallback,
-  useMemo,
-} from 'react';
+import { ComponentType, CSSProperties, useCallback, useMemo } from 'react';
 import {
   AutoSizer,
+  Index,
   InfiniteLoader,
   List,
   ListRowProps,
@@ -25,57 +21,70 @@ export interface InfiniteListProps<T> {
   data: T[];
   totalElements: number;
   rowHeight?: number;
-  rowRenderer: ComponentType<ListTypeProps<T>>;
-  emptyRenderer?: ComponentType;
+  rowComponent: ComponentType<ListTypeProps<T>>;
+  emptyComponent?: ComponentType;
   overscanRowCount?: number;
-  // react-query
+  // for react-query
   hasNextPage?: boolean;
   isFetchingNextPage: boolean;
   fetchNextPage: () => Promise<any>;
-  bottomLoaderRender?: ComponentType;
+  bottomLoaderComponent?: ComponentType;
 }
 
 export function InfiniteList<T>({
   data,
   totalElements, // 추가하면 좋은 속성
   rowHeight = 100,
-  rowRenderer: RowComponent,
-  emptyRenderer: EmptyComponent,
-  overscanRowCount = 10,
+  rowComponent: RowComponent,
+  emptyComponent: EmptyComponent,
+  overscanRowCount = 0,
+  // for react-query
   hasNextPage,
   isFetchingNextPage,
   fetchNextPage,
-  bottomLoaderRender: LoaderComponent,
+  bottomLoaderComponent: BottomLoaderComponent,
 }: InfiniteListProps<T>) {
-  // hasNext
   const rowCount = useMemo(() => {
-    // loader를 list 범위에 추가하기 위해서 hasNextPage를 체크해서 true면 1을 추가함
-    // 마지막 index = last one은 항상 loader
-    return data.length + {hasNextPage ? 1 : 0};
+    return hasNextPage ? data.length + 1 : data.length;
   }, [data, hasNextPage]);
 
-  const onRowRenderer = useCallback(
-    (props: ListRowProps): JSX.Element => {
-      // props.index의 마지막값이 undefined인 경우에만 bottomloader를 렌더 
-      // 왜냐면? 원래 index.length에서 loader를 추가하기 위해 +1를 했기 때문에 마지막 인덱스의 값은 undefined임.
-      if (data.length === data[props.index] + 1) {
-        return (
-          <RowComponent
-            key={props.index}
-            index={props.index}
-            item={data[props.index]!}
-            style={props.style}
-          />
-        );
-      }
-      return <LoaderComponent />;
+  const isRowLoaded = useCallback(
+    ({ index }: Index): boolean => {
+      return !!data[index];
     },
     [data],
   );
 
-  // loadMoreRows를 추가하여 무조건적으로 fetchNextPage가 실행되지 않도록 함 
-  // 이유는> 불필요한 상황에서도 렌더링될 수 있음 최소한의 에러도 없애기 위해서 
-  // hasNextPage와 isFetchingNextPage를 체크해서 fetchNextPage가 실행되어야 하는 상황을 체크해서 조건부로 렌더링함
+  const onBottomLoaderRenderer = useCallback((): JSX.Element => {
+    if (BottomLoaderComponent) {
+      return <BottomLoaderComponent />;
+    }
+    return <DefaultLoader />;
+  }, [BottomLoaderComponent]);
+
+  const onRowRenderer = useCallback(
+    (props: ListRowProps): JSX.Element => {
+      if (data[props.index] === undefined) {
+        return onBottomLoaderRenderer();
+      }
+      return (
+        <RowComponent
+          key={props.index}
+          index={props.index}
+          item={data[props.index]!}
+          style={props.style}
+        />
+      );
+    },
+    [data],
+  );
+
+  // eslint-disable-next-line consistent-return
+  const loadMoreRows = useCallback(() => {
+    if (!isFetchingNextPage && hasNextPage) {
+      return fetchNextPage();
+    }
+  }, [isFetchingNextPage, hasNextPage]);
 
   const onEmptyRenderer = useCallback((): JSX.Element => {
     if (EmptyComponent) {
@@ -84,51 +93,36 @@ export function InfiniteList<T>({
     return <EmptyView />;
   }, [EmptyComponent]);
 
-  const onLoaderRenderer = useCallback((): JSX.Element => {
-    if (LoaderComponent) {
-      return <LoaderComponent />;
-    }
-    return <DefaultLoader />;
-  }, [LoaderComponent]);
-
-  const isRowLoaded = useCallback(
-    (index: number): boolean => {
-      return !!data[index];
-    },
-    [data],
-  );
-
   return (
     <WindowScroller>
-      {({ height, isScrolling, scrollTop }) => (
-        <>
-          <InfiniteLoader
-            isRowLoaded={isRowLoaded}
-            loadMoreRows={fetchNextPage}
-            rowCount={rowCount}
-          >
-            {({ onRowsRendered, registerChild }) => (
-              <AutoSizer disableHeight>
-                {({ width }) => (
-                  <List
-                    ref={registerChild}
-                    height={height}
-                    width={width}
-                    isScrolling={isScrolling}
-                    scrollTop={scrollTop}
-                    onRowsRendered={onRowsRendered}
-                    rowCount={rowCount}
-                    rowHeight={rowHeight}
-                    rowRenderer={onRowRenderer}
-                    noRowsRenderer={onEmptyRenderer}
-                    overscanRowCount={overscanRowCount}
-                  />
-                )}
-              </AutoSizer>
-            )}
-          </InfiniteLoader>
-          {isFetchingNextPage && onLoaderRenderer}
-        </>
+      {({ height, isScrolling, scrollTop, onChildScroll }) => (
+        <InfiniteLoader
+          isRowLoaded={isRowLoaded}
+          loadMoreRows={loadMoreRows}
+          rowCount={rowCount}
+        >
+          {({ onRowsRendered, registerChild }) => (
+            <AutoSizer disableHeight>
+              {({ width }) => (
+                <List
+                  ref={registerChild}
+                  autoHeight
+                  height={height}
+                  width={width}
+                  isScrolling={isScrolling}
+                  scrollTop={scrollTop}
+                  onScroll={onChildScroll}
+                  onRowsRendered={onRowsRendered}
+                  rowCount={rowCount}
+                  rowHeight={rowHeight}
+                  rowRenderer={onRowRenderer}
+                  noRowsRenderer={onEmptyRenderer}
+                  overscanRowCount={overscanRowCount}
+                />
+              )}
+            </AutoSizer>
+          )}
+        </InfiniteLoader>
       )}
     </WindowScroller>
   );
