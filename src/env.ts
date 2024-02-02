66b5d3f328b33bde11442a4c98c7a03e581ba54e
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-02-02 21:25:57 +0900

    Chore: env.ts에 NEXT_PUBLIC_NAVER_CLIENT_ID 타입 추가

--- src/env.ts
+++ src/env.ts
@@ -3,14 +3,16 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
-  NEXT_PUBLIC_WEB_DOMAIN: z.string(),
-  NEXT_PUBLIC_BACKEND_URL: z.string(),
+  NEXT_PUBLIC_WEB_DOMAIN: z.string().url(),
+  NEXT_PUBLIC_BACKEND_URL: z.string().url(),
+  NEXT_PUBLIC_NAVER_CLIENT_ID: z.string(),
 });
 
 const envParsed = envSchema.safeParse({
   NODE_ENV: process.env.NODE_ENV,
   NEXT_PUBLIC_WEB_DOMAIN: process.env.NEXT_PUBLIC_WEB_DOMAIN,
   NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  NEXT_PUBLIC_NAVER_CLIENT_ID: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
 });
 
 if (!envParsed.success) {
