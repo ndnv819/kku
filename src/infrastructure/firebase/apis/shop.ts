107f53b23edb63bce063c2cd8e5ce15e8fca47ea
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-02-05 20:53:47 +0900

    Feat: firestore의 add, put, patch, delete 함수 추가

--- src/infrastructure/firebase/apis/shop.ts
+++ src/infrastructure/firebase/apis/shop.ts
@@ -1,6 +1,9 @@
 import { doc, getDoc, getDocs, query } from '@firebase/firestore';
 import { shopsCollection } from '@infrastructure/firebase/collections';
 
+import { deleteDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { omit } from 'lodash';
+import { db } from '../firestore';
 import type { Shop } from '../models/shop';
 
 export async function getShops(): Promise<Shop[]> {
@@ -17,4 +20,46 @@ export async function getShopById(id: string): Promise<Shop> {
     return docSnap.data();
   }
   throw new Error('존재하지 않는 상점입니다.');
+}
+
+export async function addShop(shop: Omit<Shop, 'id'>): Promise<Shop> {
+  try {
+    const docRef = doc(shopsCollection);
+    await setDoc(docRef, shop);
+    return {id: docRef.id, ...shop};
+  } catch (error) {
+    throw new Error('상점 추가에 실패했습니다.');
+  }
+}
+
+// NOTE:: 덮어씌움
+export async function putShop(shop: Shop): Promise<Shop> {
+  try {
+    const docRef = doc(shopsCollection, shop.id);
+    await setDoc(docRef, omit(shop, ['id']));
+    return shop;
+  } catch (error) {
+    throw new Error('상점 수정에 실패했습니다.');
+  }
+}
+
+export async function patchShop(id: string, shop: Partial<Shop>): Promise<Shop> {
+  try {
+    const docRef = doc(shopsCollection, id);
+    await updateDoc(docRef, shop)
+
+    const patcheDoc = await getDoc(docRef);
+    return patcheDoc.data()!;
+  } catch (error) {
+    throw new Error('상점 수정에 실패했습니다.');
+  }
+}
+
+export async function deleteShop(id: string): Promise<void> {
+  try {
+    const docRef = (doc(db, "shop", id))
+    await deleteDoc(docRef);
+  } catch (error) {
+    throw new Error('존재하지 않는 상점입니다.');
+  }
 }
