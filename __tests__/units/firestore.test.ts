4550c60bed0bdf877e90da45725e91d421a79877
JIAH LEE <89337955+ndnv819@users.noreply.github.com>
2024-02-25 15:07:44 +0900

    Test: react-query, firestore test 코드 수정

---
+++ __tests__/units/firestore.test.ts
@@ -0,0 +1,3 @@
+import 'firebase/firestore';
+
+describe('firestore', () => {});
--- __tests__/units/react.query.test.tsx
+++ __tests__/units/react.query.test.tsx
@@ -1,13 +1,7 @@
 import { useBaseQuery } from '@application/hooks/api/base/use_base_query';
-import { requestGet } from '@infrastructure/network';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
-import nock from 'nock';
 
-// ref: https://tanstack.com/query/latest/docs/framework/react/guides/testing#testing-network-calls
-// TODO:: testing useBaseQuery, useBaseMutation
-// TODO:: change axios to firestore ?
-// NOTE:: 확장자가 tsx여야 jsx 문법을 인식하고 처리할 수 있음
 const queryClient = new QueryClient();
 const wrapper = ({ children }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
@@ -15,31 +9,20 @@ const wrapper = ({ children }) => (
 function useTestHook() {
   const { data, isError, isFetching, isLoading } = useBaseQuery({
     queryKey: ['test'],
-    queryFn: () => requestGet('/todos/1'),
+    queryFn: jest.fn(),
   });
+
+  console.log('useTestHook', data);
 
   return { data, isError, isFetching, isLoading };
 }
 
 describe('[react-query]', () => {
-  test('mocking data를 주입하여 200일 때, useBaseQuery가 올바른 data를 리턴하는지 확인', async () => {
-    nock('https://jsonplaceholder.typicode.com').get('/todos/1').reply(200, {
-      userId: 1,
-      id: 1,
-      title: 'delectus aut autem',
-      completed: false,
-    });
+  test('jest.fn을 통해 useBaseQuery가 제대로 작동하는지 확인', async () => {
     const { result } = renderHook(() => useTestHook(), { wrapper });
 
     await waitFor(() => {
-      return result.current.data;
+      expect(result.current.data).; // 
     });
-
-    expect(result.current.data).toEqual({
-      userId: 1,
-      id: 1,
-      title: 'delectus aut autem',
-      completed: false,
-    }); // undefined
   });
 });
