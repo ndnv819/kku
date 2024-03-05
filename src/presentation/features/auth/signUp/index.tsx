8111799bf749ff8a3647d8e8b1f0a494615bb1ad
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-05 23:06:05 +0900

    Feat: signIn 버튼 추가

--- src/presentation/features/auth/signUp/index.tsx
+++ src/presentation/features/auth/signUp/index.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Button } from '@presentation/components/atoms/button';
+import { signIn } from 'next-auth/react';
+import { useCallback } from 'react';
+
 export function SignUp(): JSX.Element {
+  const handleSignUp = useCallback(async (): Promise<void> => {
+    await signIn('kakao');
+  }, []);
+
   return (
     <section>
-      <h1>회원가입</h1>
+      <Button onClick={handleSignUp}>카카오톡 간편 회원가입</Button>
     </section>
   );
 }
