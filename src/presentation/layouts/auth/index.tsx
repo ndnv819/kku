517925a7dd94b255ec24269c31f458ebe62a6508
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-05 23:06:24 +0900

    Feat: AuthLayout 내부에서 session 체크

--- src/presentation/layouts/auth/index.tsx
+++ src/presentation/layouts/auth/index.tsx
@@ -1,7 +1,22 @@
+// eslint-disable-next-line simple-import-sort/imports
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+import { useEffect } from 'react';
 import { BaseLayout } from '../base';
 import type { AuthLayoutProps } from './types';
 
 export function AuthLayout({ children }: AuthLayoutProps): JSX.Element {
   // TODO:: next-auth session 유무 체크, 없으면 signUp page routing
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      router.replace('/auth/signUp');
+    }
+  }, [session]);
+
   return <BaseLayout>{children}</BaseLayout>;
 }
