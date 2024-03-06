0d4951ea8f7b5f92e9d04e8376e9c3bf0fcd3984
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-06 16:24:34 +0900

    Improve: 중복 코드 parseJson()로 분리

---
+++ src/application/helpers/json/parse_json.ts
@@ -0,0 +1,11 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import type { ShopDTO } from 'src/pages/api/shop/dtos';
+
+export const parseJson = async (): Promise<ShopDTO[]> => {
+  const jsonPath = path.join(process.cwd(), 'scripts/flattened_array.json');
+  const jsonFile = await fsPromises.readFile(jsonPath, 'utf-8');
+  const result = JSON.parse(jsonFile) as ShopDTO[];
+
+  return result;
+};
--- src/pages/shop/[id].tsx
+++ src/pages/shop/[id].tsx
@@ -1,8 +1,7 @@
+import { parseJson } from '@application/helpers/json/parse_json';
 import { ShopDetail } from '@presentation/features/shopDetail';
 import { BaseLayout } from '@presentation/layouts/base';
-import fsPromises from 'fs/promises';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import path from 'path';
 
 import type { ShopDTO } from '../api/shop/dtos';
 
@@ -21,9 +20,7 @@ const ShopDetailPage: NextPage<ShopDetailPageProps> = ({
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const jsonPath = path.join(process.cwd(), 'scripts/flattened_array.json');
-  const jsonFile = await fsPromises.readFile(jsonPath, 'utf-8');
-  const result = JSON.parse(jsonFile) as ShopDTO[];
+  const result = await parseJson();
   const paths = result.map((s) => ({
     params: { id: s.id },
   }));
@@ -43,9 +40,8 @@ export const getStaticProps: GetStaticProps<ShopDetailPageProps> = async (
       notFound: true,
     };
   }
-  const jsonPath = path.join(process.cwd(), 'scripts/flattened_array.json');
-  const jsonFile = await fsPromises.readFile(jsonPath, 'utf-8');
-  const result = JSON.parse(jsonFile) as ShopDTO[];
+
+  const result = await parseJson();
   const shop = result.find((r) => r.id === params.id);
 
   if (!shop) {
