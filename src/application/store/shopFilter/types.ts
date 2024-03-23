be0cce5c947844c6152788dfb7d367a48120c312
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-24 00:17:07 +0900

    Comment: zod 관련 주석

--- src/application/store/shopFilter/types.ts
+++ src/application/store/shopFilter/types.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const ShopTypeEnumZod = z.enum(['전체', '카페', '식당']);
+// zod의 infer와 typescript의 typeof를 이용하면 이미 정의한 스키마로부터 타입을 추론할 수 있다
 export type ShopTypeEnum = z.infer<typeof ShopTypeEnumZod>;
 
 const ShopFilterZod = z.object({
