b3ae90e6a9163fd75a635cee8ed3e87f5d828673
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-24 00:11:06 +0900

    Design: css 수정

--- src/presentation/layouts/base/index.tsx
+++ src/presentation/layouts/base/index.tsx
@@ -3,8 +3,6 @@ import type { JSX } from 'react';
 
 import styles from './base.module.scss';
 
-export function BaseLayout(props: BaseLayoutProps): JSX.Element {
-  const { children } = props;
-
+export function BaseLayout({ children }: BaseLayoutProps): JSX.Element {
   return <div className={styles['base-layout-wrapper']}>{children}</div>;
 }
