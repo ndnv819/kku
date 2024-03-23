021003201720e44e5c13c8581b7072a77ce7af27
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-23 23:59:05 +0900

    Design: ListView 디자인 및 마크업 요소 수정

--- src/presentation/components/organism/listView/index.tsx
+++ src/presentation/components/organism/listView/index.tsx
@@ -2,22 +2,14 @@ import { ListItem } from '@presentation/components/molecules/listItem';
 
 import type { ListViewProps } from './types';
 
-export function ListView({ shops }: ListViewProps): JSX.Element | null {
-  if (!shops) {
-    return null;
-  }
-
+export function ListView({ shops }: ListViewProps): JSX.Element {
   return (
-    <ul className="px-[16px]">
-      {shops.map((shop) => {
+    <ul className="divide-y px-[16px]">
+      {shops!.map((shop) => {
         return (
-          <ListItem
-            key={shop.id}
-            id={shop.id}
-            name={shop.name}
-            category={shop.category}
-            address={shop.address}
-          />
+          <li key={shop!.id} className="relative p-base-layout">
+            <ListItem shop={shop} />
+          </li>
         );
       })}
     </ul>
