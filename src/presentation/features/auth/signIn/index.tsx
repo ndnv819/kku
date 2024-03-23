53a95eacfd0c5abc8f28c9c7b6b792a926fefa7b
Jiah Lee <89337955+ndnv819@users.noreply.github.com>
2024-03-24 00:02:58 +0900

    Feat: 카카오 로그인 버튼 추가

--- src/presentation/features/auth/signIn/index.tsx
+++ src/presentation/features/auth/signIn/index.tsx
@@ -1,7 +1,34 @@
+/* eslint-disable simple-import-sort/imports */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Button } from '@presentation/components/atoms/button';
+import { Appbar } from '@presentation/components/organism/appbar';
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+import { useCallback } from 'react';
+
 export function SignIn(): JSX.Element {
+  const handleSignIn = useCallback(async (): Promise<void> => {
+    await signIn('kakao', { callbackUrl: '/' });
+  }, []);
+
   return (
-    <section>
-      <h1>로그인</h1>
-    </section>
+    <>
+      <Appbar>
+        <Appbar.BackButton />
+        <Appbar.Title title="로그인" />
+        <Appbar.Dummy />
+      </Appbar>
+      <section className="flex h-[100vh] flex-col items-center justify-center">
+        <Button onClick={handleSignIn} className="gap-[8px] bg-[#FEE500]">
+          <Image
+            src="/assets/icons/ic_kakao.svg"
+            width={24}
+            height={24}
+            alt="kakao"
+          />
+          <p>카카오톡 간편 회원가입</p>
+        </Button>
+      </section>
+    </>
   );
 }
